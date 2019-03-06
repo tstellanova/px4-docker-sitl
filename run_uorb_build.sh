4af8f4b55128a2405ec213fc1da458921f1fe99b
Todd Stellanova <tstellanova@users.noreply.github.com>
2019-03-06 16:49:18 +0000

    Fix typo

--- run_uorb_build.sh
+++ run_uorb_build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-docker build -f Dockerfile_build_px4_urob_aarch64 -t px4_uorb_build .
+docker build -f Dockerfile_build_px4_uorb_aarch64 -t px4_uorb_build .
 # creates a shared proj directory where firmware sources can live
 mkdir -p $PWD/proj
 docker run --rm --name px4_build1 -v $PWD/proj:/proj -it px4_uorb_build
