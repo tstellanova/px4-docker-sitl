aef31f3c1d4d0dcfb0d51fea3fc3f88057eb8dc6
tstellanova <tstellanova@users.noreply.github.com>
2019-03-06 18:03:12 -0800

    simplify uorb sitl build

--- run_uorb_build.sh
+++ run_uorb_build.sh
@@ -4,3 +4,4 @@ docker build -f Dockerfile_build_px4_uorb_aarch64 -t px4_uorb_build .
 # creates a shared proj directory where firmware sources can live
 mkdir -p $PWD/proj
 docker run --rm --name px4_build1 -v $PWD/proj:/proj -it px4_uorb_build
+docker build -f Dockerfile_sitl_uorb_px4_aarch64 -t px4_uorb_sitl .
