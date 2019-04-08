0043d64de667ca4b2d92473a580520da34a1b0ff
Todd Stellanova <tstellanova@users.noreply.github.com>
2019-04-08 14:15:19 +0000

    Update to new trunk sidecar branch

--- entrypoint-sitl-uorb-build.sh
+++ entrypoint-sitl-uorb-build.sh
@@ -3,7 +3,7 @@
 mkdir -p $PWD/proj
 cd $PWD/proj
 if [ ! -d "./px4firmware-uorb" ]; then
-  git clone --branch sitl_sidecar_rebuild  https://github.com/tstellanova/px4firmware px4firmware-uorb
+  git clone --branch tcs_sidecar_trunk https://github.com/tstellanova/px4firmware px4firmware-uorb
 fi
 cd px4firmware-uorb
 make px4_sitl 
